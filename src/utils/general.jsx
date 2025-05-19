540c772eed1a3b1721c621d5c5e42ccd65f62658
Filip Siri <filipsiri03@hotmail.com>
2025-05-19 18:37:58 +0200

    Fix image url

--- src/utils/general.jsx
+++ src/utils/general.jsx
@@ -1,4 +1,4 @@
 export const GetImageUrl = (avatarUri) => {
-  const baseUrl = "https://localhost:7061/api/v1/user/avatar";
+  const baseUrl = "https://api.filipsiri.se/api/v1/user/avatar";
   return avatarUri ? `${baseUrl}/${avatarUri}` : `${baseUrl}/default.png`;
 };
