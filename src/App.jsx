3ab0f0cfacaed06d77760edde152480a3f7f4b7a
Filip Siri <filipsiri03@hotmail.com>
2025-05-21 20:28:33 +0200

    Fix mobile zoom on inputs

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,20 @@ function App() {
     setSidebarOpen(prevState => !prevState);
   }
 
+  useEffect(() => {
+  const setVh = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+
+  setVh();
+  window.addEventListener('resize', setVh);
+
+  return () => {
+    window.removeEventListener('resize', setVh);
+  };
+}, []);
+
   return (
     <>
       <ToastParent />
